f9ff8a5cab0b7b2319c2e229f01cc658118690f5
DevPan29 <darenagel@hotmail.it>
2024-04-17 07:45:45 +0200

    added contact module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ContactModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
